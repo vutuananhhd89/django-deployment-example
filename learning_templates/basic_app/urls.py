992e168d12164909a4c344a81deb733f684bd971
Adam <vutuananh@hd89@gmail.com>
2018-09-28 11:57:19 +0700

    first commit

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    re_path(r'^relative/$', views.relative, name ='relative'),
+    re_path(r'^other/$',views.other, name='other'),
+]
